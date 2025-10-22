7164695998caa36bd7afc514c7a3975277874080
Aropiosham <aroshamsham26@gmail.com>
2025-10-22 09:35:57 +0530

    added commments in feature

---
+++ src/test/java/comments/Addemp.java
@@ -0,0 +1,11 @@
+//package Tests;
+//import org.testng.annotations.Test;
+//
+//public class AddEmployeeTests extends BaseTest {
+//
+//    @Test
+//    public void addEmployee() {
+//        test.info("Adding a new employee...");
+//        test.pass("Employee added successfully!");
+//    }
+//}
